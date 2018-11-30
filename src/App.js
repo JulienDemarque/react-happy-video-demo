87321a2e98784f58106b8e810fd2359fe3bd6199
julien demarque <julien_demarque@hotmail.fr>
2018-11-30 17:45:21 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import VideoPlayer from "react-happy-video";
+import "./App.css";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <h1>react-happy-video demo</h1>
+        <div className="video-player">
+          <VideoPlayer
+            width="100%"
+            color="#3b3346"
+            source="https://s3.us-east-2.amazonaws.com/freecodecamp-hackaton/videos-for-testing/Dunes1.mp4"
+          />
+        </div>
+        <div className="video-player">
+          <VideoPlayer
+            width="100%"
+            color="#FF5216"
+            source="https://s3.us-east-2.amazonaws.com/freecodecamp-hackaton/videos-for-testing/Dunes1.mp4"
+          />
+        </div>
+        <div className="video-player">
+          <VideoPlayer
+            width="100%"
+            color="#0F9F4F"
+            source="https://s3.us-east-2.amazonaws.com/freecodecamp-hackaton/videos-for-testing/Dunes1.mp4"
+          />
+        </div>
+        <div className="video-player">
+          <VideoPlayer
+            width="100%"
+            color="#F8B700"
+            source="https://s3.us-east-2.amazonaws.com/freecodecamp-hackaton/videos-for-testing/Dunes1.mp4"
+          />
+        </div>
       </div>
     );
   }
